ba19ae8e1083b92529f5feb55905904b214ad125
RonnyK36 <ronnykelvyne3@gmail.com>
2022-09-04 19:40:30 +0300

    Added delete note, store in local storage and auto-load notes on app launch

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
 const newNoteBtn = document.getElementById('add-note')
 
+const notes = JSON.parse(localStorage.getItem('notes'))
+
+if (notes) {
+    notes.forEach(note => {
+        addNewNote(note)
+    });
+}
+
+
 newNoteBtn.addEventListener('click', () => {
     addNewNote()
 })
 
-function addNewNote() {
+function addNewNote(text = '') {
     const note = document.createElement('div')
     note.classList.add('notes')
     note.innerHTML = `
@@ -13,8 +22,8 @@ function addNewNote() {
             <button class="edit"><i class="fas fa-edit"></i></button>
             <button class="delete"><i class="fas fa-trash-alt"></i></button>
         </div>
-        <div class="main hidden"></div>
-        <textarea></textarea>
+        <div class="main ${text ? '' : 'hidden'}"></div>
+        <textarea class="${text ? 'hidden' : ''}"></textarea>
     </div>
     `
     const editBtn = note.querySelector('.edit')
@@ -23,6 +32,8 @@ function addNewNote() {
     const mainEl = note.querySelector('.main')
     const textAreaEl = note.querySelector('textarea')
 
+    textAreaEl.value = text
+    mainEl.innerHTML = marked(text)
 
     editBtn.addEventListener('click', () => {
         mainEl.classList.toggle('hidden')
@@ -31,12 +42,32 @@ function addNewNote() {
 
     deleteBtn.addEventListener('click', () => {
         note.remove()
+        updateLocalStorage()
     })
 
     textAreaEl.addEventListener('input', (event) => {
         const { value } = event.target
 
         mainEl.innerHTML = marked(value)
+
+        updateLocalStorage()
     })
     document.body.appendChild(note)
 }
+
+
+function updateLocalStorage() {
+    const notesText = document.querySelectorAll('textarea')
+
+    const notes = []
+
+    notesText.forEach(note => {
+        notes.push(note.value)
+    });
+
+    localStorage.setItem('notes', JSON.stringify(notes))
+}
+
+// background: rgb(236, 215, 215);
+// ## New Test Note
+// This is a dummy note
